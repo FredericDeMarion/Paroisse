d59809bb347c2aca833a02f478f7da870706c7f0
FredericDeMarion <frederic.de.marion@free.fr>
2017-11-01 18:09:44 +0100

    Bug affichage liste événement
    Conséquence de la mise en place des célébrations récurrentes

--- Evenements.php
+++ Evenements.php
@@ -14,6 +14,7 @@
 // 28/05/2017 : La dernière ligne de la requête n'est pas affichée dans le listing général
 // 17/07/2017 : Ajout de la fonction Gerer_Equipe_Technique_Messe
 // 30/09/2017 : Problème d'affichage du mauvais célébrant dans la liste générale
+// 26/10/2017 : Affichage du dernier événement pendant 5h
 //==================================================================================================
 
 if (isset( $_SERVER['PHP_AUTH_USER'] ) AND $_SERVER['PHP_AUTH_USER'] == "celebration"){
@@ -1107,8 +1108,10 @@ if ( isset( $_POST['Prog_Recurrente_Celebration_sauvegarder'] ) AND
 	$debug = False;
 	pCOM_DebugInit($debug);
 	
+	//----------------------------------------------------
 	// Réinitialiser la liste des prochaines célébrations
 	// Date limite = today + 60 jours
+	//---------------------------------------------------
 	$DateLimite=fCOM_sqlDateToOut(date("Y-m-d H:i:s")) + (24*3600*360);
 	$CurrentDate=date("Y-m-d");
 	pCOM_DebugAdd($debug,"Evenements:Liste DateLimite=".$DateLimite);
@@ -1133,7 +1136,7 @@ if ( isset( $_POST['Prog_Recurrente_Celebration_sauvegarder'] ) AND
 			if ($DateRec >= fCOM_sqlDateToOut($row['DateDeb']) AND 
 				$DateRec <= fCOM_sqlDateToOut($row['DateFin']) AND
 				$DateRec <= $DateLimite ) {
-				$requete = 'INSERT INTO Celebrations_futur (Date, Lieu_id) VALUES ("'.date("Y-m-d",$DateRec).'", '.$row['Lieu_id'].')';
+				$requete = 'INSERT INTO Celebrations_futur (Date, Lieu_id) VALUES ("'.date("Y-m-d H:i:s",$DateRec).'", '.$row['Lieu_id'].')';
 				pCOM_DebugAdd($debug,"Evenements:Liste Requete02=".$requete);
 			//pCOM_DebugAdd($debug, "Evenement:ProgRecCelSave - requete=".$requete);
 				mysqli_query($eCOM_db, $requete) or die (mysqli_error($eCOM_db));
@@ -1172,71 +1175,85 @@ if ( isset( $_POST['Prog_Recurrente_Celebration_sauvegarder'] ) AND
 	$MemoParoissien = "";
 	
 	$requete = '(SELECT T0.`id` as id, T0.`Date` as Date, T0.`Intitule` as Intitule, T1.`Lieu` as Lieu, CONCAT(T3.`Prenom`, " ", T3.`Nom`) as Celebrant, CONCAT(T5.`Prenom`, " ", T5.`Nom`) as Sacristin, CONCAT(T7.`Prenom`, " ", T7.`Nom`) as Animateur, CONCAT(T9.`Prenom`, " ", T9.`Nom`) as Musicien, CONCAT(T11.`Prenom`, " ", T11.`Nom`) as EveilFoi, CONCAT(T13.`Prenom`, " ", T13.`Nom`) as Garderie, "Messe" AS Activité, "-" AS Paroissien, "OK" AS Status, 0 as Ordre, CONCAT(T15.`Prenom`, " ", T15.`Nom`) as Sono, CONCAT(T17.`Prenom`, " ", T17.`Nom`) as Projection, CONCAT(T19.`Prenom`, " ", T19.`Nom`) as Broadcast   
-				FROM Rencontres T0
-				LEFT JOIN `Lieux` T1 ON T0.`Lieux_id` = T1.`id`
-				LEFT JOIN `QuiQuoi` T2 ON T0.`id` = T2.`Engagement_id` AND T2.`QuoiQuoi_id`=5  AND T2.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T3 ON T2.`Individu_id` = T3.`id`
-				LEFT JOIN `QuiQuoi` T4 ON T0.`id` = T4.`Engagement_id` AND T4.`QuoiQuoi_id`=12 AND T4.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T5 ON T4.`Individu_id` = T5.`id`
-				LEFT JOIN `QuiQuoi` T6 ON T0.`id` = T6.`Engagement_id` AND T6.`QuoiQuoi_id`=13 AND T6.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T7 ON T6.`Individu_id` = T7.`id`
-				LEFT JOIN `QuiQuoi` T8 ON T0.`id` = T8.`Engagement_id` AND T8.`QuoiQuoi_id`=21 AND T8.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T9 ON T8.`Individu_id` = T9.`id`
-				LEFT JOIN `QuiQuoi` T10 ON T0.`id` = T10.`Engagement_id` AND T10.`QuoiQuoi_id`=16 AND T10.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T11 ON T10.`Individu_id` = T11.`id`
-				LEFT JOIN `QuiQuoi` T12 ON T0.`id` = T12.`Engagement_id` AND T12.`QuoiQuoi_id`=23 AND T12.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T13 ON T12.`Individu_id` = T13.`id`
-				LEFT JOIN `QuiQuoi` T14 ON T0.`id` = T14.`Engagement_id` AND T14.`QuoiQuoi_id`=24 AND T14.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T15 ON T14.`Individu_id` = T15.`id`
-				LEFT JOIN `QuiQuoi` T16 ON T0.`id` = T16.`Engagement_id` AND T16.`QuoiQuoi_id`=25 AND T16.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T17 ON T16.`Individu_id` = T17.`id`
-				LEFT JOIN `QuiQuoi` T18 ON T0.`id` = T18.`Engagement_id` AND T18.`QuoiQuoi_id`=26 AND T18.`Activite_id`= T0.`Activite_id`
-				LEFT JOIN `Individu` T19 ON T18.`Individu_id` = T19.`id`
-				WHERE T0.`Activite_id`=86 AND T0.`Date`> (NOW() - INTERVAL 3 MONTH))
-				UNION ALL
-				(SELECT T0.`id` as id, T0.`Date` as Date, "Baptême" as Intitule, T1.`Lieu` as Lieu, Concat(T5.`Prenom`, " ",T5.`Nom`) AS Celebrant, "" as Sacristin, "" as Animateur, "" as Musicien, "" as EveilFoi, "" as Garderie, T0.`Aspersion_Immersion` AS Activité,  Concat(T3.`Prenom`, " ",T3.`Nom`) AS Paroissien, "OK" AS Status, 1 as Ordre, "" as Sono, "" as Projection, "" as Broadcast 
-				FROM `Bapteme` as T0 
-				LEFT JOIN `Lieux` as T1 ON T0.`Lieu_id`=T1.`id` 
-				LEFT JOIN `Individu` as T3 ON T3.`id`=T0.`Baptise_id` 
-				LEFT JOIN `Individu` as T5 ON T5.`id`=T0.`Celebrant_id` 
-				WHERE T1.`IsParoisse` = 1 AND T0.`Date`>=(NOW() - INTERVAL 3 MONTH))
-				UNION ALL
-				(SELECT T4.`id` as id, T4.`Date_mariage` as Date, "Mariage" as Intitule, T4.`Lieu_mariage` as Lieu, IFNULL(Concat(T8.`Prenom`, " ", T8.`Nom`), IF(T4.`Celebrant_id`=0, "En Attente", IF(T4.`Celebrant_id`=-1 AND T4.`Celebrant`="", "Célébrant Extérieur", T4.`Celebrant`))) AS Celebrant, 
-				"" as Sacristin, "" as Animateur, "" as Musicien, "" as EveilFoi, "" as Garderie, "Mariage" AS Activité, 
-				CONCAT(
-				(SELECT concat( T6.`Prenom` , " ", T6.`Nom` )
-				FROM Individu T6
-				LEFT JOIN `QuiQuoi` AS T7 ON T7.`Individu_id` = T6.`id`
-				WHERE T6.`Sex` = "F" AND T7.`QuoiQuoi_id` =1 AND T7.`Activite_id` =2 AND T7.`Engagement_id` = T4.`id`), " et ",
-				(SELECT concat( T6.`Prenom` , " ", T6.`Nom` )
-				FROM Individu T6
-				LEFT JOIN `QuiQuoi` AS T7 ON T7.`Individu_id` = T6.`id`
-				WHERE T6.`Sex` = "M" AND T7.`QuoiQuoi_id` =1 AND T7.`Activite_id` =2 AND T7.`Engagement_id` = T4.`id`)) As Paroissien, T4.`Status` AS Status, 2 as Ordre, "" as Sono, "" as Projection, "" as Broadcast
-				FROM `Fiancés` as T4
-				LEFT JOIN `Individu` AS T8 ON T8.`id`=T4.`Celebrant_id`
-				LEFT JOIN `Lieux` as T9 ON T9.`Lieu`=T4.`Lieu_mariage` 
-				WHERE T9.`IsParoisse` = 1 AND T4.`Date_mariage` >= (NOW() - INTERVAL 3 MONTH))
-				UNION ALL
-				(SELECT "0" as id, T0.`date` as Date, "" as Intitule, T1.`Lieu` as Lieu, "" AS Celebrant, "" as Sacristin, "" as Animateur, "" as Musicien, "" as EveilFoi, "" as Garderie, "Messe Rec" AS Activité,  "" AS Paroissien, "OK" AS Status, -1 as Ordre, "" as Sono, "" as Projection, "" as Broadcast 
-				FROM `Celebrations_futur` as T0 
-				LEFT JOIN `Lieux` as T1 ON T0.`Lieu_id`=T1.`id` 
-				WHERE T0.`date`>=now())
-				ORDER BY '.$criteria.' '.$order.' '.$SecondCriteria; //Date, Lieu, Ordre';
+		FROM Rencontres T0
+		LEFT JOIN `Lieux` T1 ON T0.`Lieux_id` = T1.`id`
+		LEFT JOIN `QuiQuoi` T2 ON T0.`id` = T2.`Engagement_id` AND T2.`QuoiQuoi_id`=5  AND T2.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T3 ON T2.`Individu_id` = T3.`id`
+		LEFT JOIN `QuiQuoi` T4 ON T0.`id` = T4.`Engagement_id` AND T4.`QuoiQuoi_id`=12 AND T4.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T5 ON T4.`Individu_id` = T5.`id`
+		LEFT JOIN `QuiQuoi` T6 ON T0.`id` = T6.`Engagement_id` AND T6.`QuoiQuoi_id`=13 AND T6.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T7 ON T6.`Individu_id` = T7.`id`
+		LEFT JOIN `QuiQuoi` T8 ON T0.`id` = T8.`Engagement_id` AND T8.`QuoiQuoi_id`=21 AND T8.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T9 ON T8.`Individu_id` = T9.`id`
+		LEFT JOIN `QuiQuoi` T10 ON T0.`id` = T10.`Engagement_id` AND T10.`QuoiQuoi_id`=16 AND T10.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T11 ON T10.`Individu_id` = T11.`id`
+		LEFT JOIN `QuiQuoi` T12 ON T0.`id` = T12.`Engagement_id` AND T12.`QuoiQuoi_id`=23 AND T12.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T13 ON T12.`Individu_id` = T13.`id`
+		LEFT JOIN `QuiQuoi` T14 ON T0.`id` = T14.`Engagement_id` AND T14.`QuoiQuoi_id`=24 AND T14.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T15 ON T14.`Individu_id` = T15.`id`
+		LEFT JOIN `QuiQuoi` T16 ON T0.`id` = T16.`Engagement_id` AND T16.`QuoiQuoi_id`=25 AND T16.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T17 ON T16.`Individu_id` = T17.`id`
+		LEFT JOIN `QuiQuoi` T18 ON T0.`id` = T18.`Engagement_id` AND T18.`QuoiQuoi_id`=26 AND T18.`Activite_id`= T0.`Activite_id`
+		LEFT JOIN `Individu` T19 ON T18.`Individu_id` = T19.`id`
+		WHERE T0.`Activite_id`=86 AND T0.`Date`> (NOW() - INTERVAL 3 MONTH))
+				
+		UNION ALL
+				
+		(SELECT T0.`id` as id, T0.`Date` as Date, "Baptême" as Intitule, T1.`Lieu` as Lieu, Concat(T5.`Prenom`, " ",T5.`Nom`) AS Celebrant, "" as Sacristin, "" as Animateur, "" as Musicien, "" as EveilFoi, "" as Garderie, T0.`Aspersion_Immersion` AS Activité,  Concat(T3.`Prenom`, " ",T3.`Nom`) AS Paroissien, "OK" AS Status, 1 as Ordre, "" as Sono, "" as Projection, "" as Broadcast 
+		FROM `Bapteme` as T0 
+		LEFT JOIN `Lieux` as T1 ON T0.`Lieu_id`=T1.`id` 
+		LEFT JOIN `Individu` as T3 ON T3.`id`=T0.`Baptise_id` 
+		LEFT JOIN `Individu` as T5 ON T5.`id`=T0.`Celebrant_id` 
+		WHERE T1.`IsParoisse` = 1 AND T0.`Date`>=(NOW() - INTERVAL 3 MONTH))
+			
+		UNION ALL
+				
+		(SELECT T4.`id` as id, T4.`Date_mariage` as Date, "Mariage" as Intitule, T4.`Lieu_mariage` as Lieu, IFNULL(Concat(T8.`Prenom`, " ", T8.`Nom`), IF(T4.`Celebrant_id`=0, "En Attente", IF(T4.`Celebrant_id`=-1 AND T4.`Celebrant`="", "Célébrant Extérieur", T4.`Celebrant`))) AS Celebrant, "" as Sacristin, "" as Animateur, "" as Musicien, "" as EveilFoi, "" as Garderie, "Mariage" AS Activité, 
+		CONCAT((SELECT concat( T6.`Prenom` , " ", T6.`Nom` )
+			FROM Individu T6
+			LEFT JOIN `QuiQuoi` AS T7 ON T7.`Individu_id` = T6.`id`
+			WHERE T6.`Sex` = "F" AND T7.`QuoiQuoi_id` =1 AND T7.`Activite_id` =2 AND T7.`Engagement_id` = T4.`id`), " et ",
+			(SELECT concat( T6.`Prenom` , " ", T6.`Nom` )
+		FROM Individu T6
+		LEFT JOIN `QuiQuoi` AS T7 ON T7.`Individu_id` = T6.`id`
+		WHERE T6.`Sex` = "M" AND T7.`QuoiQuoi_id` =1 AND T7.`Activite_id` =2 AND T7.`Engagement_id` = T4.`id`)) As Paroissien, T4.`Status` AS Status, 2 as Ordre, "" as Sono, "" as Projection, "" as Broadcast
+		FROM `Fiancés` as T4
+		LEFT JOIN `Individu` AS T8 ON T8.`id`=T4.`Celebrant_id`
+		LEFT JOIN `Lieux` as T9 ON T9.`Lieu`=T4.`Lieu_mariage` 
+		WHERE T9.`IsParoisse` = 1 AND T4.`Date_mariage` >= (NOW() - INTERVAL 3 MONTH))
+				
+		UNION ALL
+				
+		(SELECT "0" as id, T0.`date` as Date, "" as Intitule, T1.`Lieu` as Lieu, "" AS Celebrant, "" as Sacristin, "" as Animateur, "" as Musicien, "" as EveilFoi, "" as Garderie, "Messe Rec" AS Activité,  "" AS Paroissien, "OK" AS Status, -1 as Ordre, "" as Sono, "" as Projection, "" as Broadcast 
+		FROM `Celebrations_futur` as T0 
+		LEFT JOIN `Lieux` as T1 ON T0.`Lieu_id`=T1.`id` 
+		WHERE T0.`date`>=now())
+		ORDER BY '.$criteria.' '.$order.' '.$SecondCriteria; //Date, Lieu, Ordre';
 
+	$MemoDateOrg="";
 	$result = mysqli_query( $eCOM_db, $requete);
 	$number = mysqli_num_rows($result);
-	$PremLine = true;
+	$PremLine = 1;
 	$i = 1;
 	while($row = mysqli_fetch_assoc($result) OR ($i <= $number+1 AND $number > 0)){
 		
+		pCOM_DebugAdd($debug, "Evenements:liste - id=".$row['id']);
+		pCOM_DebugAdd($debug, "Evenements:liste - MemoDateOrg=".$MemoDateOrg." New date=".$row['Date']);
+		pCOM_DebugAdd($debug, "Evenements:liste - MemoLieu=".$MemoLieu." New lieu=".$row['Lieu']);
+		pCOM_DebugAdd($debug, "Evenements:liste - PremLine=".$PremLine);
+		pCOM_DebugAdd($debug, "Evenements:liste - Status=".$row['Status']);
+		pCOM_DebugAdd($debug, "Evenements:liste - i=".$i." number+1=".$number+1);
+		
+		// --------------------------------------------------------------
 		// Print de la ligne si on est pas à la 1ère ligne de la table
+		// --------------------------------------------------------------
 		if ( ($MemoLieu != $row['Lieu'] OR $MemoDateOrg != $row['Date'] OR $i == $number+1) AND 
-			$PremLine == false AND $row['Status'] != "Annulé/Reporté") {
+			$PremLine == 0 AND $row['Status'] != "Annulé/Reporté") {
 			
 			$trcolor = usecolor();
 			
 			pCOM_DebugAdd($debug, "Evenements:liste - cas 1");
-			if ($MemoDateOrg < date("Y-m-d H:i:s")) {
+			if ($MemoDateOrg < date("Y-m-d H:i:s", Time() - (5*3600))) { // on laisse en visibilité pendant 5h
 				$ColorFont="#919191";
 				$compteur = $compteur + 1;
 				echo '<h6 style="display:none;"></h6><TR id="Filtrer_'.$compteur.'" style="display:none;">';		
@@ -1368,12 +1385,16 @@ if ( isset( $_POST['Prog_Recurrente_Celebration_sauvegarder'] ) AND
 				$MemoId = $row['id'];
 			}		
 			echo '</TR>';
+			pCOM_DebugAdd($debug, "Evenements:liste - MemoSacristin=".$MemoSacristin." New Sacristin=".$row['Sacristin']);
 			
-		} elseif ( $PremLine == True AND $row['Status'] != "Annulé/Reporté") {
+		} elseif ( $PremLine == 1 AND $row['Status'] != "Annulé/Reporté") {
 			
+		// --------------------------------------------------------------
+		// Il s'agit de la première ligne de la table
+		// --------------------------------------------------------------
 			pCOM_DebugAdd($debug, "Evenements:liste - cas 2");
 			//if ($MemoActivité == "") {
-				if ($row['Ordre']== -1 OR $row['Ordre']==0) { // Messe ou Messe Rec
+				if ($row['Ordre']== -1 OR $row['Ordre'] == 0) { // Messe ou Messe Rec
 					if ($MemoActivité == $MemoIntitule AND strlen($MemoIntitule) > 0 ) {
 						$MemoActivité = "";
 					} else {
@@ -1386,7 +1407,7 @@ if ( isset( $_POST['Prog_Recurrente_Celebration_sauvegarder'] ) AND
 				}
 			//}
 			$MemoIntitule = Build_Intitule("", $row['Ordre'], $MemoActivité, $row['id'], $row['Paroissien'], $row['Celebrant']);
-			$PremLine = False;
+			$PremLine = 0;
 			$MemoDate = strftime("%d/%m/%Y  %H:%M", sqlDateToOut($row['Date']));
 			$MemoDisplayDate = strftime("%d/%m/%y  %a%H:%M", sqlDateToOut($row['Date']));
 			$MemoDateOrg = $row['Date'];
@@ -1406,29 +1427,41 @@ if ( isset( $_POST['Prog_Recurrente_Celebration_sauvegarder'] ) AND
 			}			
 			
 		} else {
-			// Date et Lieu sont équivalents au précédent
+		// --------------------------------------------------------------
+		// Date et Lieu sont équivalents au précédent
+		// --------------------------------------------------------------
 			
 			pCOM_DebugAdd($debug, "Evenements:liste - cas 3");
-			//if ($MemoActivité == "") {
-				if ($row['Ordre']== -1 OR $row['Ordre']==0) { // Messe ou Messe Rec
-					if ($MemoActivité == $MemoIntitule AND strlen($MemoIntitule) > 0) {
-						$MemoActivité = "";
-					} else {
-						$MemoActivité = $row['Intitule'];
-					}				
-				} elseif ($row['Ordre']== 2) { // "Mariage"
-					$MemoActivité = $row['Activité'];
-				} elseif ($row['Ordre']== 1) { // "Baptême" 
-					$MemoActivité = $Liste_Type_Bapteme[$row['Activité']];
-				}
-			//}
+			if ($row['Ordre']== -1 OR $row['Ordre']==0) { // Messe ou Messe Rec
+				if ($MemoActivité == $MemoIntitule AND strlen($MemoIntitule) > 0) {
+					$MemoActivité = "";
+				} else {
+					$MemoActivité = $row['Intitule'];
+				}				
+			} elseif ($row['Ordre']== 2) { // "Mariage"
+				$MemoActivité = $row['Activité'];
+			} elseif ($row['Ordre']== 1) { // "Baptême" 
+				$MemoActivité = $Liste_Type_Bapteme[$row['Activité']];
+			}
 			$MemoIntitule = Build_Intitule($MemoIntitule, $row['Ordre'], $MemoActivité, $row['id'], $row['Paroissien'], $row['Celebrant']);
+			
+			if ($row['Celebrant'] != ""){$MemoCelebrant = $row['Celebrant'];}
+			if ($row['Sacristin'] != ""){$MemoSacristin = $row['Sacristin'];}
+			if ($row['EveilFoi'] != ""){$MemoEveilFoi = $row['EveilFoi'];}
+			if ($row['Garderie'] != ""){$MemoGarderie = $row['Garderie'];}
+			if ($row['Sono'] != ""){$Team_sono = $row['Sono'];}
+			if ($row['Projection'] != ""){$Team_projection = $row['Projection'];}
+			if ($row['Broadcast'] != ""){$Team_broadcast = $row['Broadcast'];}
 			
 			if ($row['Animateur'] != "" AND $MemoAnimateur != "" AND strpos($MemoAnimateur, $row['Animateur'])===False) {
 				$MemoAnimateur = $MemoAnimateur.' et<BR>'.$row['Animateur'];
+			} elseif ($MemoAnimateur == "") {
+				$MemoAnimateur = $row['Animateur'];
 			}
 			if ($row['Musicien'] != "" AND $MemoMusicien != "" AND strpos($MemoMusicien, $row['Musicien'])===False) {
 				$MemoMusicien = $MemoMusicien.' et<BR>'.$row['Musicien'];
+			} elseif ($MemoMusicien == "") {
+				$MemoMusicien = $row['Musicien'];
 			}
 			$MemoOrdre = $row['Ordre']; // 0-Messe, 1-Baptême, 2-Mariage
 			if ($MemoOrdre == 0){ // != Baptême et Mariage
@@ -1444,11 +1477,15 @@ if ( isset( $_POST['Prog_Recurrente_Celebration_sauvegarder'] ) AND
 function Build_Intitule ($pMemoIntitule, $pType, $pIntitule, $pId, $pParoissien, $pCelebrant) {
 	Global $Activite_id;
 	$Build_Intitule = $pMemoIntitule;
-	if (strlen($Build_Intitule) > 0 AND strlen($pIntitule) > 0) { $Build_Intitule = $Build_Intitule."<BR>"; }
-	if ($pType==0) {
+	if (strlen($Build_Intitule) > 0 AND strlen($pIntitule) > 0 AND $Build_Intitule!=$pIntitule) { $Build_Intitule = $Build_Intitule."<BR>"; }
+	if ($pType == 0  AND $Build_Intitule!=$pIntitule) {
 		$Build_Intitule = $Build_Intitule.$pIntitule;
 	} elseif ($pType==1 OR $pType==2) { // Cas du Mariage et Baptême
-		$Build_Intitule = $Build_Intitule.$pCelebrant." célèbre le ".$pIntitule." de ";
+		if ($pCelebrant != "En Attente") {
+			$Build_Intitule = $Build_Intitule.$pCelebrant." célèbre le ".$pIntitule." de ";
+		} else {
+			$Build_Intitule = $Build_Intitule.$pIntitule." de ";
+		}
 	}
 	if (fCOM_Get_Autorization( $Activite_id ) >= 30 || 
 		fCOM_Get_Autorization( 51 ) >= 30 || 
